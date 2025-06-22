8bb6b6a5531fdcd7c7549377bc37222f7621ddd4
Luka Lomtatidze <luka.lomtatidze@iliauni.edu.ge>
2025-06-22 21:35:50 +0400

    2.3 push

--- Chapter2_basics/2.3.py
+++ Chapter2_basics/2.3.py
@@ -1,3 +1,8 @@
+"""Exercise 2.3: Write a program to perform the inverse operation to that of Example 2.2.
+That is, ask the user for the Cartesian coordinates x, y of a point in two-dimensional
+space, and calculate and print the corresponding polar coordinates, with the angle 9
+given in degrees."""
+
 from math import sin, cos, pi, asin, acos, sqrt, atan
 
 x = float(input('Enter x:'))
